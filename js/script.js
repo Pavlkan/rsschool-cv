652a5743e30c37d9bb242cdeb98b729ee7a804a2
Pavlkan <PavelKarman.Pavlkan@gmail.com>
2021-12-27 21:06:37 +0300

    feat: add script

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+
+function changeItemContact(obj) {
+    document.getElementById(obj.id + '_underline-stretched').style.width = '320px';
+}
+
+function rechangeItemContact(obj) {
+    document.getElementById(obj.id + '_underline-stretched').style.width = '50px';
+}
