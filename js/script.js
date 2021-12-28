b7cf815b26d2c08ba833f8d482d97e34095f4c49
Pavlkan <PavelKarman.Pavlkan@gmail.com>
2021-12-28 22:33:28 +0300

    feat: add code example section with script

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 
 
 
-function changeItemContact(obj) {
+function changeUnderlineWidth(obj) {
     document.getElementById(obj.id + '_underline-stretched').style.width = '320px';
 }
 
-function rechangeItemContact(obj) {
+function rechangeUnderlineWidth(obj) {
     document.getElementById(obj.id + '_underline-stretched').style.width = '50px';
+}
+
+function changeCodeBackground(obj) {
+    document.getElementById('code-example__content').style.transition = '0.5s'; 
+    document.getElementById('code-example__content').style.background = 'rgb(40, 43, 46)';
+    document.getElementById('code-example__content').style.color = 'rgb(224, 226, 228)';
+    document.getElementById('code-example__content').style.borderRadius = '2%';
+}
+
+function rechangeCodeBackground(obj) {
+    document.getElementById('code-example__content').style.background = '#F5FFFA';
+    document.getElementById('code-example__content').style.color = 'black'
 }
