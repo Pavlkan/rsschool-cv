d2ad8cada8916db8ee95dceb11ca124f4e64bfda
Pavlkan <PavelKarman.Pavlkan@gmail.com>
2021-12-31 23:27:59 +0300

    add: progects section content

--- js/script.js
+++ js/script.js
@@ -20,3 +20,18 @@ function rechangeCodeBackground(obj) {
     document.getElementById('code-example__content').style.background = '#F5FFFA';
     document.getElementById('code-example__content').style.color = 'black'
 }
+
+let slides = document.getElementsByClassName('progect-container');
+    for (let i=0; i < slides.length; ++i) {
+        slides[i].onclick = function () {
+            let child = this.childNodes;
+            for (let cI in child) {
+                // console.log(child[cI])
+                if (child[cI].className == 'progects__content') {
+                    let d = child[cI].style.display;
+                    child[cI].style.display = d === 'block' ? 'none' : 'block';
+                }
+            }
+        }
+    }
+
